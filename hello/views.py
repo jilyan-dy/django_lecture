17340734bb84b01907cfb88c52c9fc978bd62080
snowy-night <jilyan_dy@dlsu.edu.ph>
2022-05-12 17:14:16 +0800

    render a template instead of just a string

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,17 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# using a template to render a html file
+# prefix templates with directory name for namespace
 def index(request):
-	return HttpResponse("Hello!")
+	return render(request, "hello/index.html")
 
-def brian(request):
-	return HttpResponse("Hello, Brian!")
+# def brian(request):
+# 	return HttpResponse("Hello, Brian!")
 
-def greet(reqquest, name):
-	return HttpResponse(f"Hello, {name}!")
+def greet(request, name):
+	return render(request, "hello/greet.html", {
+		"name": name.capitalize()
+	})
