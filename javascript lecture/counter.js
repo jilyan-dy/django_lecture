5ffadc653f36c11ae506cef57bddc528b71f3a60
snowy-night <jilyan_dy@dlsu.edu.ph>
2022-07-01 20:51:45 +0800

    Utilizing local storage

--- javascript lecture/counter.js
+++ javascript lecture/counter.js
@@ -1,7 +1,12 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+	localStorage.setItem('counter', 0);
+}
+
 function increment() {
+	let counter = localStorage.getItem('counter');
 	counter++;
 	document.querySelector('h1').innerHTML = 'Count:' + counter;
+	localStorage.setItem('counter', counter);
 
 	if (counter % 10 === 0) {
 		alert(`Counter has reached ${counter}`);
@@ -9,5 +14,9 @@ function increment() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+	document.querySelector('h1').innerHTML = 'Count:' + localStorage.getItem('counter');
 	document.querySelector('button').onclick = increment;
+
+	// auto increment
+	// setInterval(interval, 1000); 
 });
